7fdd48e48a231953042e791f60f0b82d2f14f73e
Liny Thomas <linythomas2@gmail.com>
2024-09-08 20:41:22 +0400

    End of Section 10

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -14,7 +14,7 @@ export class ShopService {
   brands: string[] = [];
   shopParams = new ShopParams();
 
-  getProducts(shopParams:ShopParams) {
+  getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
     if (shopParams.brands.length > 0) {
       params = params.append('brands', shopParams.brands.join(','));
@@ -24,16 +24,17 @@ export class ShopService {
     }
     params = params.append('pageSize', shopParams.pageSize);
     params = params.append('pageIndex', shopParams.pageNumber);
-    if (shopParams.sort)
-      params = params.append('sort', shopParams.sort);
-    if (shopParams.search)
-      params=params.append('search',shopParams.search)
+    if (shopParams.sort) params = params.append('sort', shopParams.sort);
+    if (shopParams.search) params = params.append('search', shopParams.search);
 
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products', {
       params,
     });
   }
-
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+  
   getBrands() {
     if (this.brands.length > 0) return;
     return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe({
